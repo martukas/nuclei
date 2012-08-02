f5a445c0d942c1183813b235f54a5f90a5122348
mnagl <mnagl@4e419536-2157-47c4-8b72-7732f613270d>
2012-08-02 22:38:33 +0000

    Version bump Removed redundant delete command

--- macfix.sh
+++ macfix.sh
@@ -4,5 +4,4 @@ cp -r /opt/local/lib/Resources/qt_menu.nib Nuclei.app/Contents/Resources
 install_name_tool -change /usr/lib/libakk.1.dylib @executable_path/../Frameworks/libakk.1.dylib Nuclei.app/Contents/MacOS/nuclei
 sed -e "s/com.yourcompany/de.uni-goettingen/g" Nuclei.app/Contents/Info.plist > tmp.plist
 mv tmp.plist Nuclei.app/Contents/Info.plist
-rm tmp.plist
 hdiutil create ./Nuclei.dmg -srcfolder ./Nuclei.app/ -ov
