6f0ae78d2cf00ee46e35a945aa2804cb62711968
MGS <martukas@gmail.com>
2016-12-20 00:28:50 +0100

    parse even more shit

--- ensdf/ensdf_records.cpp
+++ ensdf/ensdf_records.cpp
@@ -59,6 +59,10 @@ RecordType IdentificationRecord::is_type(std::string s)
     return RecordType::HiXng;
 
   RecordType ret = RecordType::Invalid;
+  if (ReactionData::match(s))
+    ret |= RecordType::Reaction;
+  if (boost::contains(str, "INELASTIC SCATTERING"))
+    ret |= RecordType::InelasticScattering;
   if (boost::contains(str, "COULOMB EXCITATION"))
     ret |= RecordType::CoulombExcitation;
   if (boost::contains(str, "ADOPTED LEVELS"))
@@ -79,10 +83,10 @@ std::string IdentificationRecord::type_to_str(RecordType t)
     return "COMMENTS";
   else if (test(t & RecordType::References))
     return "REFERENCES";
-  else if (test(t & RecordType::CoulombExcitation))
-    return "COULOMB EXCITATION";
   else if (test(t & RecordType::MuonicAtom))
     return "MUONIC ATOM";
+  else if (test(t & RecordType::Invalid))
+    return "INVALID";
   else if (test(t & RecordType::HiXng))
     return "(HI,XNG)";
 
@@ -90,6 +94,12 @@ std::string IdentificationRecord::type_to_str(RecordType t)
 
   if (test(t & RecordType::AdoptedLevels))
     ret += "ADOPTED LEVELS";
+  if (test(t & RecordType::CoulombExcitation))
+    ret += "COULOMB EXCITATION";
+  if (test(t & RecordType::InelasticScattering))
+    ret += "INELASTIC SCATTERING";
+  if (test(t & RecordType::Reaction))
+    ret += "REACTION";
   if (test(t & RecordType::Decay))
     ret += "DECAY";
   if (test(t & RecordType::Gammas))
@@ -158,7 +168,8 @@ std::string ParentRecord::to_string() const
 
 BasicDecayData BasicDecayData::from_id(const IdentificationRecord &record, BlockIndices block)
 {
-  if ((record.type & RecordType::Decay) == RecordType::Invalid)
+  if (((record.type & RecordType::Decay) == RecordType::Invalid) &&
+      ((record.type & RecordType::InelasticScattering) == RecordType::Invalid))
   {
     DBG << "Bad ID record type " << record.debug();
     //HACK (Tentative should propagate to NucData)
@@ -290,23 +301,169 @@ ReactionData ReactionData::from_id(const IdentificationRecord &record, BlockIndi
   ret.block = block;
   ret.daughter = record.nuc_id;
 
-//  std::list<std::string> tokens;
+  std::string nuclide = "[\\w\\d]+";
+  std::string reactant = "[\\s\\w\\d'\\+-]+";
+  std::string in_out = "\\(" + reactant + "," + reactant + "\\)";
+  std::string target_reactions = nuclide + "\\s*" + in_out + "(?:\\s*,\\s*" + in_out + ")*";
+  std::string splitxtras = "^(" + target_reactions + "(?:\\s*,\\s*" + target_reactions + "\\s*)*)(.*)$";
 
-//  boost::regex decay_expr("^[([\\sA-Z0-9]+[\\([\\sA-Z0-9]+\\),*\\s+]+)[, ]*]+([E=[\\sA-Z0-9]+]*)");
+  std::string xtions, extras;
+//  std::string xtras;
 
-//  boost::regex decay_expr("^[([\\sA-Z0-9]+[\\([\\sA-Z0-9]+\\),*\\s+]+)[, ]*]");
+//  DBG << "Will parse " << record.debug();
 
-//  boost::smatch what;
-//  if (boost::regex_match(boost::trim_copy(record.extended_dsid),
-//                         what, decay_expr) && (what.size() > 0))
-//    for (auto w : what)
-//      DBG << "   " << w;
+  boost::regex xtions_xtras(splitxtras);
+  boost::smatch what;
+  if (boost::regex_match(boost::trim_copy(record.extended_dsid), what, xtions_xtras) && (what.size() > 1))
+  {
+//    DBG << " good";
+    xtions = what[1];
+    if (what.size() > 2)
+      extras = what[2];
+  }
 
+//  DBG << " Xions " << xtions;
+
+  std::string split_xxtions = "^\\s*(" + target_reactions + ")(?:\\s*,\\s(" + target_reactions + "))*.*$";
+  boost::regex xtions_exp(split_xxtions);
+  boost::smatch what2;
+  boost::regex_match(xtions, what2, xtions_exp);
+  for (int i=1; i < what2.size(); ++i)
+  {
+    std::string parse_xtions = "^(" + nuclide + ")\\s*(" + in_out + ")(?:\\s*,\\s*(" + in_out + "))*$";
+    boost::regex xtion_exp(parse_xtions);
+    boost::smatch what3;
+
+//    DBG << "  xoion \"" << what2[i] << "\"";
+    std::string layout = what2[i];
+    std::string layout2;
+    for (auto z =0; z < layout.size(); ++z)
+      layout2 += "\"" + std::string(1,layout.at(z)) + "\" ";
+
+//    DBG << "        " << layout2;
+
+    if (boost::regex_match(std::string(what2[i]), what3, xtion_exp) && (what3.size() > 2))
+    {
+//      for (auto xx =0; xx < what3.size(); ++xx)
+//        DBG << "    " << xx << "=" << what3[xx];
+
+      Reaction reaction;
+//      DBG << "   nucid=" << what3[1];
+      reaction.target = parse_nid(what3[1]);
+      if (!reaction.target.composition_known() && reaction.target.Z())
+        reaction.target.set_A(ret.daughter.A());
+
+      for (int j=2; j < what3.size(); ++j)
+      {
+//        DBG << "   ants " << what3[j];
+
+        std::string variant = "^\\((" + reactant + "),(" + reactant + ")\\)$";
+        boost::regex variant_exp(variant);
+        boost::smatch what4;
+        if (boost::regex_match(std::string(what3[j]), what4, variant_exp) && (what4.size() > 2))
+        {
+          Reactants reactants;
+          reactants.in = what4[1];
+          reactants.out = what4[2];
+          reaction.variants.push_back(reactants);
+        }
+      }
+      ret.reactions.push_back(reaction);
+    }
+  }
+
+  if (ret.find_remove(extras, "E=", "E=") ||
+      ret.find_remove(extras, "E =", "E =") ||
+      ret.find_remove(extras, "E<", "E") ||
+      ret.find_remove(extras, "E>", "E") ||
+      ret.find_remove(extras, "E AP", "E "))
+  {
+    boost::trim(ret.energy);
+    std::size_t found = ret.energy.find("E=");
+    if (found!=std::string::npos)
+      ret.energy.erase(found, 2);
+    else
+      ret.energy.erase(0,1);
+    boost::trim(ret.energy);
+  }
+
+  boost::trim(extras);
+  std::size_t found = ret.energy.find(":");
+  if (found!=std::string::npos)
+    ret.energy.erase(found, 1);
+  boost::trim(extras);
+
+  ret.qualifier = extras;
+
+  return ret;
+}
+
+bool ReactionData::find_remove(std::string& extras, std::string wanted, std::string trim_what)
+{
+  std::size_t found = extras.find(wanted);
+  if (found!=std::string::npos)
+  {
+    energy = extras.substr(found, extras.size()-found);
+    extras.erase(found, extras.size()-found);
+    found = energy.find(trim_what);
+    if (found!=std::string::npos)
+      energy.erase(found, trim_what.size());
+    boost::trim(energy);
+    return true;
+  }
+  return false;
+}
+
+
+std::string ReactionData::Reactants::to_string() const
+{
+  return "(" + in + "," + out + ")";
+}
+
+std::string ReactionData::Reaction::to_string() const
+{
+  std::string ret = boost::trim_copy(nid_to_ensdf(target, false));
+  bool first = true;
+  for (auto x : variants)
+  {
+    if (!first)
+      ret += ",";
+    if (first)
+      first = false;
+    ret += x.to_string();
+  }
   return ret;
 }
 
 std::string ReactionData::to_string() const
 {
   std::string ret;
+  ret = daughter.verboseName();
+  ret += "   dsid=\"" + dsid + "\"  ";
+  bool first = true;
+  for (auto r : reactions)
+  {
+    if (!first)
+      ret += ",";
+    if (first)
+      first = false;
+    ret += r.to_string();
+  }
+  ret += "  q=\"" + qualifier + "\"  ";
+//  if (!energy.empty())
+  ret += energy;
   return ret;
+}
+
+bool ReactionData::match(std::string record)
+{
+  std::string nuclide = "[\\w\\d]+";
+  std::string reactant = "[\\s\\w\\d'\\+-]+";
+  std::string in_out = "\\(" + reactant + "," + reactant + "\\)";
+  std::string target_reactions = nuclide + "\\s*" + in_out + "(?:\\s*,\\s*" + in_out + ")*";
+  std::string splitxtras = "^(" + target_reactions + "(?:\\s*,\\s*" + target_reactions + "\\s*)*)(.*)$";
+
+  boost::smatch what;
+  boost::regex filter(splitxtras);
+  return boost::regex_match(boost::trim_copy(record), what, filter);
 }
