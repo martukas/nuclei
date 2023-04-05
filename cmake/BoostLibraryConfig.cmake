fb7153914432d13a229f4fdb8e5a327890898039
Martin Shetty <1972005+martukas@users.noreply.github.com>
2023-04-05 01:13:52 -0400

    Dependency updates and CircleCI job (#5)
    * updated conan packages, cleaned up unused cmake features, updated readme; updates #1
    
    * circleci config; updates #1
    
    * circleci config2; updates #1
    
    * circleci config 3; updates #1
    
    * circleci config 4; updates #1
    
    * circleci config 5; updates #1
    
    * circleci config 6; updates #1
    
    * circleci config 7; updates #1
    
    * less boost; updates #1

--- cmake/BoostLibraryConfig.cmake
+++ cmake/BoostLibraryConfig.cmake
@@ -2,8 +2,9 @@ set(Boost_USE_STATIC_LIBS OFF)
 set(Boost_USE_MULTITHREADED ON)
 set(Boost_USE_STATIC_RUNTIME OFF)
 
-find_package(Boost COMPONENTS
-  filesystem system regex algorithm REQUIRED)
+set(Boost_DEBUG ON)
+
+find_package(Boost COMPONENTS regex REQUIRED)
 
 if (CMAKE_CXX_COMPILER_ID MATCHES MSVC)
   add_definitions(-DBOOST_ALL_DYN_LINK)
